d000813d7310137f543b445534908e93fe3bd5e6
Eric Chengyu Liu <chengyu.liu@ericsson.com>
2016-07-26 15:30:56 +0300

    update lib/config.py
    created functionality of getting host IP, setting listen_address, rpc_address, seed IP.

--- lib/config.py
+++ lib/config.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import yaml
 import os
+import netifaces
+
 
 HOME_CASSANDRA = "/opt/tools/cassandra-2.2.7"
 DIR = os.path.dirname(__file__)
@@ -9,6 +11,19 @@ CASSANDRA_PARAMETER = os.path.join(DIR, '../conf/cassandra.yaml')
 CASSANDRA_CONFIG = os.path.join(HOME_CASSANDRA, 'conf/cassandra.yaml')
 
 class Cassandra(object):
+    def __init__(self, template=CASSANDRA_TEMPLATE, parameter=CASSANDRA_PARAMETER, config=CASSANDRA_CONFIG, interface='eth0'):
+        self.file_config = config
+        self.config = self.read_yaml(template)
+        self.parameter = self.read_yaml(parameter)
+        self.ip = self.get_host_ip(interface)
+        #def _lazy_setting(self):
+        self.config = self.set_yaml(self.config, self.parameter)
+        self.config = self.set_yaml(self.config, {'listen_address': self.ip})
+        self.config = self.set_yaml(self.config, {'rpc_address': self.ip})
+        self.set_seeds_ip()
+        self.write_yaml(self.file_config, self.config)
+    def set_seeds_ip(self):
+        self.config['seed_provider'][0]['parameters'][0]['seeds'] = self.ip
     def read_yaml(self, file):
         with open(file) as f:
             try:
@@ -16,22 +31,23 @@ class Cassandra(object):
             except:
                 raise
         return data
-
     def set_yaml(self, template, parameter):
         templ_kesy = template.keys()
         param_keys = parameter.keys()
         for key in param_keys:
             template[key] = parameter[key]
         return template
-
     def write_yaml(self, file, data):
         with open(file, 'w') as f:
             yaml.dump(data, f, allow_unicode=True,default_flow_style=False)
+    def get_host_ip(self, interface='eth0'):
+        try:
+            return netifaces.ifaddresses(interface)[2][0]['addr']
+        except ValueError:
+            print "Could not find interface {0}".format(interface)
+            return None
+
+
 if __name__ == "__main__":
-    cassandra = Cassandra()
-    template = cassandra.read_yaml(CASSANDRA_TEMPLATE)
-    parameter = cassandra.read_yaml(CASSANDRA_PARAMETER)
-    config = cassandra.set_yaml(template, parameter)
-    cassandra.write_yaml(CASSANDRA_CONFIG, config)
-#listen_address: 192.168.122.238
-#rpc_address: 192.168.122.238
+    cassandra = Cassandra(config="../conf/new.yaml", interface='eth1')
+    #cassandra = Cassandra(template="template.yaml", parameter="cassandra.yaml",config='new.yaml', interface='eth1')
