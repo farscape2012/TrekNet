73a17d1e094682b284c76a76e0c3be116ac3e8e1
Eric Chengyu Liu <chengyu.liu@ericsson.com>
2016-07-26 17:03:12 +0300

    Added a function that enables troubling connection (cassandra)

--- lib/config.py
+++ lib/config.py
@@ -5,6 +5,7 @@ import netifaces
 
 
 HOME_CASSANDRA = "/opt/tools/cassandra-2.2.7"
+#HOME_CASSANDRA = "/home/eijmmmp/TrekNet/"
 DIR = os.path.dirname(__file__)
 CASSANDRA_TEMPLATE = os.path.join(DIR, '../conf/template.yaml')
 CASSANDRA_PARAMETER = os.path.join(DIR, '../conf/cassandra.yaml')
@@ -21,6 +22,7 @@ class Cassandra(object):
         self.config = self.set_yaml(self.config, {'listen_address': self.ip})
         self.config = self.set_yaml(self.config, {'rpc_address': self.ip})
         self.set_seeds_ip()
+        self.enable_trouble_connection(file=os.path.join(HOME_CASSANDRA, 'conf/cassandra-env.sh'))
         self.write_yaml(self.file_config, self.config)
     def set_seeds_ip(self):
         self.config['seed_provider'][0]['parameters'][0]['seeds'] = self.ip
@@ -46,7 +48,16 @@ class Cassandra(object):
         except ValueError:
             print "Could not find interface {0}".format(interface)
             return None
-
+    def enable_trouble_connection(self, file):
+        lines = []
+        with open(file) as infile:
+            for line in infile:
+                if "JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=" in line:
+                    line = ("JVM_OPTS=\"$JVM_OPTS -Djava.rmi.server.hostname=" + self.ip + "\"" + "\n")
+                lines.append(line)
+        with open(file, 'w') as outfile:
+            for line in lines:
+                outfile.write(line)
 
 if __name__ == "__main__":
     cassandra = Cassandra(config="./conf/new.yaml", interface='eth1')
