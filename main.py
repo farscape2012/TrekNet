d7f315891fdbbf6a316763974915c8a1d76f489c
farscape2012 <farscape2012@users.noreply.github.com>
2016-07-27 14:23:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ DIR = os.path.dirname(__file__)
 LIB_DIR = os.path.join(DIR, 'lib')
 sys.path.append(LIB_DIR)
 
-from config import Cassandra
+from config import Cassandra, Titan
 
 if __name__ == "__main__":
     print "main is executing"
