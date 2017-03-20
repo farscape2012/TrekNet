7708b806700a831f33a4eb6011af90a39d3502aa
farscape2012 <farscape2012@users.noreply.github.com>
2017-03-20 12:20:40 +0200

    Update findKthSmallest.py

--- lib/utils/list/findKthSmallest.py
+++ lib/utils/list/findKthSmallest.py
@@ -1,18 +1,18 @@
-def findKthSmallest(nums, k):
+def findKthSmallest(arr, k):
     """
-    :type nums: List[int]
+    :type arr: List[int/float]
     :type k: int
-    :rtype: int
+    :rtype: int/float
         Note:
         Time complexity: O(n) on average; O(n^2) in worst case
         Space complexity: O(n) on average; O(n^2) in worst case
     """
-    if k > len(nums):
+    if k > len(arr):
         return None
-    pivot = nums[0]
-    left  = [l for l in nums if l < pivot]
-    equal = [e for e in nums if e == pivot]
-    right = [r for r in nums if r > pivot]
+    pivot = arr[0]
+    left  = [l for l in arr if l < pivot]
+    equal = [e for e in arr if e == pivot]
+    right = [r for r in arr if r > pivot]
     if k <= len(left):
         return findKthSmallest(left, k)
     elif (k - len(left)) <= len(equal):
