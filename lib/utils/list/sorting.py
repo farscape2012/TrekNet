2300055b6b3a362580e3c044404fcb95e6b47f6f
farscape2012 <farscape2012@users.noreply.github.com>
2017-03-20 12:32:47 +0200

    Update sorting.py

--- lib/utils/list/sorting.py
+++ lib/utils/list/sorting.py
@@ -1,4 +1,4 @@
-def quicksort(arr):
+def quickSort(arr):
     """ sort an array using quick sort algorithm
     :type arr: List
     :rtype: int/float    
@@ -14,3 +14,39 @@ def quicksort(arr):
     right = [x for x in arr if x > pivot]
     return qsort(left) + equal + qsort(right)
     
+def mergeSort(arr):
+    """ sort an array using merge sort algorithm
+    :type arr: List
+    :rtype: int/float    
+    Note:
+        Time complexity: O(nlog(n)) or O(n) in best-case; O(nlog(n)) on average; O(nlog(n)) in worst case
+        Space complexity: O(n) 
+    """
+    if len(arr)>1:
+        mid = len(arr)//2
+        lefthalf = arr[:mid]
+        righthalf = arr[mid:]
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i=0
+        j=0
+        k=0
+        lenleft = len(lefthalf)
+        lenright = len(righthalf)
+        while i < lenleft and j < lenright:
+            if lefthalf[i] < righthalf[j]:
+                arr[k]=lefthalf[i]
+                i=i+1
+            else:
+                arr[k]=righthalf[j]
+                j=j+1
+            k=k+1
+        while i < len(lefthalf):
+            arr[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+        while j < len(righthalf):
+            arr[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    return arr
