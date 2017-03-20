d6228584e739b8dc76a4178041a56f6d3ba82554
farscape2012 <farscape2012@users.noreply.github.com>
2017-03-20 12:21:31 +0200

    Update findMedian.py

--- lib/utils/list/findMedian.py
+++ lib/utils/list/findMedian.py
@@ -1,36 +1,36 @@
-def findMeidan(nums):
+def findMeidan(arr):
     """ Find meidan element in the list.
-    :type nums: List[int]
+    :type arr: List[int/float]
     :rtype: int / float
     Note:
         Time complexity: O(n) on average; O(n^2) in worst case
         Space complexity: O(n) on average; O(n^2) in worst case
     """
-    def findKthLargest(nums, k):
+    def findKthLargest(arr, k):
         """ Find kth largest element in the list
-        :type nums: List[int]
+        :type arr: List[int/float]
         :type k: int
-        :rtype: int
+        :rtype: int / float
         """
-        if k > len(nums):
+        if k > len(arr):
             return None
         pivot = nums[0]
-        left  = [l for l in nums if l < pivot]
-        equal = [e for e in nums if e == pivot]
-        right = [r for r in nums if r > pivot]
+        left  = [l for l in arr if l < pivot]
+        equal = [e for e in arr if e == pivot]
+        right = [r for r in arr if r > pivot]
         if k <= len(right):
             return findKthLargest(right, k)
         elif (k - len(right)) <= len(equal):
             return equal[0]
         else:
             return findKthLargest(left, k - len(right) - len(equal))
-    if len(nums) == 0 or nums is None:
+    if len(arr) == 0 or nums is None:
         return None
-    if len(nums) % 2 == 0:
-        ks = [len(nums)/2, len(nums)/2+1]
+    if len(arr) % 2 == 0:
+        ks = [len(arr)/2, len(arr)/2+1]
     else:
-        ks = [len(nums)/2 + 1]
+        ks = [len(arr)/2 + 1]
     medians = []
     for k in ks:
-        medians.append(findKthLargest(nums, k))
+        medians.append(findKthLargest(arr, k))
     return medians[0] if len(medians) == 1 else sum(medians)/2.0
