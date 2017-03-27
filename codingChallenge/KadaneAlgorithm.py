335e32c06428b613299b8829dffefc8f4581980b
farscape2012 <farscape2012@users.noreply.github.com>
2017-03-27 09:56:08 +0300

    Update KadaneAlgorithm.py

--- codingChallenge/KadaneAlgorithm.py
+++ codingChallenge/KadaneAlgorithm.py
@@ -1,11 +1,25 @@
-def kadaneAlg(arr):
+def kadaneAlg(arr, rindex=True):
     """ Kadane's algorithm to find maximum subarray given an array
     :type arr: List[int/float]
     :rtype: int 
     """
     maxv = arr[0]
-    cur_sum = arr[0]
-    for x in arr[1:]:
-        cur_sum = max(x, maxv + x)
-        maxv = cur_sum
-    return maxv
+    maxstart = maxend = 0
+    curstart = curend = 0
+    curmax = arr[0]
+    for i in xrange(1, len(arr)):
+        if arr[i] > curmax + arr[i]:
+            curstart = i
+            curend = i
+            curmax = arr[i]
+        else:
+            curmax = curmax + arr[i]
+            curend = i
+        if curmax > maxv:
+            maxstart = curstart
+            maxend = curend
+            maxv = curmax
+    if rindex:
+        return maxstart, maxend, maxv
+    else:
+        return maxv
