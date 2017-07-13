c7eb1511b414e3a435116eaeca8d3a67a62a3191
farscape2012 <farscape2012@users.noreply.github.com>
2017-07-13 23:04:35 +0300

    Create install.sh

---
+++ conf/zeppelin/install.sh
@@ -0,0 +1,8 @@
+# install oracle java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+
+sudo apt-get install wget
+sudo apt-get install oracle-java8-installer
+
+wget http://mirror.netinch.com/pub/apache/zeppelin/zeppelin-0.7.2/zeppelin-0.7.2-bin-all.tgz
